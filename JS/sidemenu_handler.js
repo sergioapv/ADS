5891664973cc1a5f49f0d2827d5501cc2b61c5b5
Eduardo-Filipe-Ferreira <72504304+Eduardo-Filipe-Ferreira@users.noreply.github.com>
2020-10-11 13:01:35 +0100

    Add files via upload

--- JS/sidemenu_handler.js
+++ JS/sidemenu_handler.js
@@ -1,10 +1,12 @@
 document.querySelector('.sidemenu_minimizer').addEventListener('click',function(){
   let sidemenu = document.querySelector('.sidemenu');
   let main_content = document.querySelector('.main_content')
+  let displayerEditor = document.querySelector('.displayerEditor')
   if (sidemenu.id === "clicked") {//quando ja esta clicado
     sidemenu.id = "not_clicked"
     main_content.style.left = '300px'
     main_content.style.width = 'calc(100% - 300px)'
+    displayerEditor.style.display = 'grid'
 
   }
   else {
@@ -12,6 +14,8 @@ document.querySelector('.sidemenu_minimizer').addEventListener('click',function(
     main_content.style.left = '50px'
     main_content.style.width = 'calc(100% - 50px)'
     main_content.style.marginLeft = '0px'
+    displayerEditor.style.display = 'none'
+
 
   }
 
