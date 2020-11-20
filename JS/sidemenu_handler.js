75a2fc52dd2dee4d8e98d230d7df844742b12484
Sérgio <sergioapvalentims@gmail.com>
2020-11-20 23:32:04 +0000

    CheckBoxes Listeners - Listening

--- JS/sidemenu_handler.js
+++ JS/sidemenu_handler.js
@@ -85,61 +85,63 @@ dimension_type.forEach((dimension) => {
   });
 });
 
-let select_all_list = document.querySelectorAll('.Dimension_selector');
-select_all_list.forEach((div) => {
-  let selectors = div.parentNode.getElementsByTagName('input');
-  // console.log(selectors);
-  for (var i = 0; i < selectors.length; i++) {
-    if (selectors[i].id === 'select_all') {
-      selectors[i].addEventListener('change',function(){
-        console.clear()
-        let all_selectors = this.parentNode.parentNode.getElementsByTagName('input');
-        if (this.checked === true) {
-          for (var i = 0; i < all_selectors.length; i++) {
-            if(all_selectors[i].checked == true && !all_selectors[i].name.includes('Select_all')){
-              var index = algorithms_chosen.indexOf(all_selectors[i].name);
-              if (index > -1) {
-                algorithms_chosen.splice(index, 1);
+function algs_chosen(){
+  let select_all_list = document.querySelectorAll('.Dimension_selector');
+  console.log(select_all_list);
+  select_all_list.forEach((div) => {
+    let selectors = div.parentNode.getElementsByTagName('input');
+    for (var i = 0; i < selectors.length; i++) {
+      if (selectors[i].id === 'select_all') {
+        selectors[i].addEventListener('change',function(){
+          console.clear()
+          let all_selectors = this.parentNode.parentNode.getElementsByTagName('input');
+          if (this.checked === true) {
+            for (var i = 0; i < all_selectors.length; i++) {
+              if(all_selectors[i].checked == true && !all_selectors[i].name.includes('Select_all')){
+                var index = algorithms_chosen.indexOf(all_selectors[i].name);
+                if (index > -1) {
+                  algorithms_chosen.splice(index, 1);
+                }
+              }
+              all_selectors[i].checked = true
+              if (!all_selectors[i].name.includes('Select_all')) {
+                algorithms_chosen.push(all_selectors[i].name);
+                console.log(algorithms_chosen);
               }
             }
-            all_selectors[i].checked = true
-            if (!all_selectors[i].name.includes('Select_all')) {
-              algorithms_chosen.push(all_selectors[i].name);
-              console.log(algorithms_chosen);
-            }
-          }
-        }
-        else {
-          for (var i = 0; i < all_selectors.length; i++) {
-            all_selectors[i].checked = false
-            if (!all_selectors[i].name.includes('Select_all')) {
-              algorithms_chosen.pop(all_selectors[i].name);
-              console.log(algorithms_chosen);
-            }
-          }
-        }
-      })
-    }
-    else {
-      let class_choice = selectors[i].parentNode;
-      let algorithms = class_choice.getElementsByTagName('input');
-      for (var j = 0; j < algorithms.length; j++) {
-        algorithms[j].addEventListener('change',function(){
-          console.clear()
-          if (this.checked === true) {
-              algorithms_chosen.push(this.name);
-              console.log(algorithms_chosen);
           }
           else {
-            let index = algorithms_chosen.indexOf(this.name);
-            if (index > -1) {
-              algorithms_chosen.splice(index,1);
+            for (var i = 0; i < all_selectors.length; i++) {
+              all_selectors[i].checked = false
+              if (!all_selectors[i].name.includes('Select_all')) {
+                algorithms_chosen.pop(all_selectors[i].name);
+                console.log(algorithms_chosen);
+              }
             }
-            console.log(algorithms_chosen);
           }
-        });
+        })
+      }
+      else {
+        let class_choice = selectors[i].parentNode;
+        let algorithms = class_choice.getElementsByTagName('input');
+        for (var j = 0; j < algorithms.length; j++) {
+          algorithms[j].addEventListener('change',function(){
+            console.clear()
+            if (this.checked === true) {
+                algorithms_chosen.push(this.name);
+                console.log(algorithms_chosen);
+            }
+            else {
+              let index = algorithms_chosen.indexOf(this.name);
+              if (index > -1) {
+                algorithms_chosen.splice(index,1);
+              }
+              console.log(algorithms_chosen);
+            }
+          });
+        }
       }
     }
-  }
 
-});
+  });
+}
